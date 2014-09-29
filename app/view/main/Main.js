94f9298386f6bc065340dbd2bb03cc12ab6ce4a4
Carolyn Huynh & Anna Valkov <student@epicodus.com>
2014-09-29 16:20:28 -0700

    additions to the background

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -16,9 +16,16 @@ Ext.define('TutorialApp.view.main.Main', {
 
     xtype: 'app-main',
 
+
+
     controller: 'main',
     viewModel: {
         type: 'main'
+    },
+       defaults: {
+        collapsible: true,
+        split: true,
+
     },
 
     layout: {
@@ -27,23 +34,112 @@ Ext.define('TutorialApp.view.main.Main', {
 
     items: [{
         xtype: 'panel',
+             collapsible: true,
+
         bind: {
             title: '{name}'
         },
         region: 'west',
-        html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
-        width: 250,
-        split: true,
-        tbar: [{
-            text: 'Logout',
-            handler: 'onClickButton'
-        }]
+            items:[
+            {
+            title: 'Child Panel 1',
+            html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
+            width: 250,
+            floatable: false,
+            height: 600,
+            collapsible: true,
+            tbar: [{
+                text: 'Logout',
+                handler: 'onClickButton'
+            }]
     },{
+        region: 'west',
+        xtype: 'panel',
+
+        title: 'Child Panel 2',
+        html: '<p>Hello!</p>',
+        collapsible: true,
+         floatable: false,
+        height: 100,
+        width: 250
+
+    }]},{
+
         region: 'center',
         xtype: 'tabpanel',
         items:[{
-            title: 'Tab 1',
-            html: '<h2>Content appropriate for the current navigation.</h2>'
+            title: 'Home',
+            html: '<h2>Our Beautiful Site</h2>'
+        },{
+            title: 'Table',
+                layout: 'fit',
+        items: [{
+            xtype: 'grid',
+            title: 'Simpsons',
+            store: {
+                fields:['name', 'email', 'phone'],
+                data:[
+                    { name: 'Lisa',  email: "lisa@simpsons.com",
+                      phone: "555-111-1224"  },
+                    { name: 'Bart',  email: "bart@simpsons.com",
+                      phone: "555-222-1234" },
+                    { name: 'Homer', email: "home@simpsons.com",
+                      phone: "555-222-1244"  },
+                    { name: 'Marge', email: "marge@simpsons.com",
+                      phone: "555-222-1254"  }
+                ],
+                proxy: {
+                    type: 'memory'
+                }
+            },
+            columns: [
+                { text: 'Name',  dataIndex: 'name' },
+                { text: 'Email', dataIndex: 'email', flex: 1},
+                { text: 'Phone', dataIndex: 'phone' }
+            ]
+        }]
+
+
+        },{
+
+            title: 'Form',
+            layout: 'fit',
+        items: [{
+            title: 'User Form',
+            height: 150,
+            width: 300,
+            bodyPadding: 10,
+            defaultType: 'textfield',
+            items: [
+                {
+                    fieldLabel: 'First Name',
+                    name: 'firstName'
+                },
+                {
+                    fieldLabel: 'Last Name',
+                    name: 'lastName'
+                },
+                {
+                    xtype: 'datefield',
+                    fieldLabel: 'Date of Birth',
+                    name: 'birthDate'
+                }
+            ],
+
+              buttons: [{
+                    text: 'Submit',
+                    formBind: true,
+                    handler: function() {
+                        var form = this.up('form').getForm(); // get the basic form
+                        if (form.isValid()) { // make sure the form contains valid data before
+                 }
+
+                 }
+                 }]
+
+
+        }]
+
         }]
     }]
 });
