b1f9d0347742661fe5e00a5e60b310a9c43f2f59
Carolyn Huynh & Anna Valkov <student@epicodus.com>
2014-09-29 10:44:34 -0700

    initial commit

---
+++ app/Application.js
@@ -0,0 +1,42 @@
+/**
+ * The main application class. An instance of this class is created by app.js when it calls
+ * Ext.application(). This is the ideal place to handle application launch and initialization
+ * details.
+ */
+Ext.define('TutorialApp.Application', {
+    extend: 'Ext.app.Application',
+
+    name: 'TutorialApp',
+
+    stores: [
+        // TODO: add global / shared stores here
+    ],
+    views: [
+        'TutorialApp.view.login.Login',
+        'TutorialApp.view.main.Main'
+    ],
+    launch: function () {
+
+        // Check whether the browser supports LocalStorage
+        // It's important to note that this type of application could use
+        // any type of storage, i.e., Cookies, LocalStorage, etc.
+        var supportsLocalStorage = Ext.supports.LocalStorage,
+            loggedIn;
+
+        if (!supportsLocalStorage) {
+
+            // Alert the user if the browser does not support localStorage
+            Ext.Msg.alert('Your Browser Does Not Support Local Storage');
+            return;
+        }
+
+        // Check to see the current value of the localStorage key
+        loggedIn = localStorage.getItem("TutorialLoggedIn");
+
+        // This ternary operator determines the value of the TutorialLoggedIn key.
+        // If TutorialLoggedIn isn't true, we display the login window,
+        // otherwise, we display the main view
+        Ext.widget(loggedIn ? 'app-main' : 'login');
+
+    }
+});
---
+++ build/temp/development/TutorialApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/epicodus/Code/tutorialApp/app.js
